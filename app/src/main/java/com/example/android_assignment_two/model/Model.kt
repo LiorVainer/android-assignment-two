ab8923701d5d5e75ae9e06b8b10cb9246ac090b8
Lior Vainer <liorvainer@gmail.com>
2025-01-19 22:36:21 +0200

    Added Students List Activity with Recycler List Adapter

---
+++ app/src/main/java/com/example/android_assignment_two/model/Model.kt
@@ -0,0 +1,19 @@
+package com.example.android_assignment_two.model
+
+class Model private constructor() {
+    val students: MutableList<Student> = ArrayList()
+    companion object {
+        val shared = Model()
+    }
+    init {
+        for (i in 0..20) {
+            val student = Student(
+                name = "Ben Shapiro $i",
+                id = i.toString(),
+                avatarUrl = "",
+                isChecked = false
+            )
+            students.add(student)
+        }
+    }
+}
