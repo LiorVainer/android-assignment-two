132d88f1466a3c0fea66990d0f160e46e003aec1
RomPollak <rompollak@gmail.com>
2025-01-22 21:09:03 +0200

    fixed style

--- app/src/main/java/com/example/android_assignment_two/activities/AddStudentActivity.kt
+++ app/src/main/java/com/example/android_assignment_two/activities/AddStudentActivity.kt
@@ -18,11 +18,6 @@ class AddStudentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_student)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val saveButton: Button = findViewById(R.id.add_student_activity_save_button);
         val cancelButton: Button = findViewById(R.id.add_student_activity_cancel_button);
--- app/src/main/java/com/example/android_assignment_two/activities/EditStudentActivity.kt
+++ app/src/main/java/com/example/android_assignment_two/activities/EditStudentActivity.kt
@@ -16,11 +16,6 @@ class EditStudentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_edit_student)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val studentId = intent.getStringExtra("STUDENT_ID")
         val students = Model.shared.students;
